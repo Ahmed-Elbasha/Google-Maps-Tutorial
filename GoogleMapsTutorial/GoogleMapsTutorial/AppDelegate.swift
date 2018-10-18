bc80d55c565888ae8e14197e5dcc76cf13cc3a79
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-10-18 12:12:01 +0200

    Configured Google Maps For iOS API  Service

--- GoogleMapsTutorial/GoogleMapsTutorial/AppDelegate.swift
+++ GoogleMapsTutorial/GoogleMapsTutorial/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyDN0myiiM4_3NTbXFeuNF1GsAFIqGy1Suw")
         return true
     }
 
